73337e7b400b8832a526f23f88c26179ae63d69a
FouMez <fira_s@hotmail.fr>
2021-05-01 15:46:26 +0300

    DB to Staging

--- src/db.js
+++ src/db.js
@@ -8,9 +8,9 @@ const mongooseOptions = {
 
 // TODO set pw to proccess.env
 // PROD
-const databaseUrl = 'mongodb://AdminKatomi:fw9spRj#mWg4hXq1@15.237.60.243:27017/msdb?authSource=admin';
+// const databaseUrl = 'mongodb://AdminKatomi:fw9spRj#mWg4hXq1@15.237.60.243:27017/msdb?authSource=admin';
 // STAGING
-// const databaseUrl = 'mongodb+srv://root:aZbOkdpzE5bfizjT@msante.vi2cn.mongodb.net/msante?retryWrites=true&w=majority';
+const databaseUrl = 'mongodb+srv://root:aZbOkdpzE5bfizjT@msante.vi2cn.mongodb.net/msante?retryWrites=true&w=majority';
 
 mongoose.connect(databaseUrl, mongooseOptions).catch((err) => {
   console.log(err);
